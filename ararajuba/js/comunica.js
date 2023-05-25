6f8d2022724d351910a42a3926a7bbb7b66bb6a2
amanda <amandafaleiro200@gmail.com>
2023-05-25 15:09:16 -0300

    coiso

--- ararajuba/js/comunica.js
+++ ararajuba/js/comunica.js
@@ -1,267 +1,267 @@
-function setOptions () {
-	var checkAndDo = function (radio, f) {
-		return ($(radio).is(":checked")) ? f(radio) : null;
-	}
+function setOptions() {
+    var checkAndDo = function(radio, f) {
+        return ($(radio).is(":checked")) ? f(radio) : null;
+    }
 
-	$("#tts").change(function (ev) {
-		checkAndDo(this, function (r) {
-			$("#sr").prop('tabindex', -1);
-			$("#tts").prop('tabindex', 0);
-			$("#tts-controls").show();
-			$("#sr-controls").hide();
-			$(".tts-controls div").removeClass('invisible');
-			$('.tts-controls select').prop('disabled', false);
-			$(".sr-controls").hide();
-		});
-	});
+    $("#tts").change(function(ev) {
+        checkAndDo(this, function(r) {
+            $("#sr").prop('tabindex', -1);
+            $("#tts").prop('tabindex', 0);
+            $("#tts-controls").show();
+            $("#sr-controls").hide();
+            $(".tts-controls div").removeClass('invisible');
+            $('.tts-controls select').prop('disabled', false);
+            $(".sr-controls").hide();
+        });
+    });
 
-	$("#sr").change(function (ev) {
-		checkAndDo(this, function (r) {
-			$("#sr").prop('tabindex', 0);
-			$("#tts").prop('tabindex', -1);
-			$("#sr-controls").show();
-			$("#tts-controls").hide();
-			$(".sr-controls").show();
-			$(".tts-controls div").addClass('invisible');
-			$('.tts-controls select').prop('disabled', true);
-			$("#text").val("");
-		});
-	});
+    $("#sr").change(function(ev) {
+        checkAndDo(this, function(r) {
+            $("#sr").prop('tabindex', 0);
+            $("#tts").prop('tabindex', -1);
+            $("#sr-controls").show();
+            $("#tts-controls").hide();
+            $(".sr-controls").show();
+            $(".tts-controls div").addClass('invisible');
+            $('.tts-controls select').prop('disabled', true);
+            $("#text").val("");
+        });
+    });
 
-} 
-
-function setSpeakFunction (voices) {
-	$("#speak").click(function (ev) {
-		var textVal = $("#text").val();
-		if (textVal === null || textVal === "") return;
-
-		var msg = new SpeechSynthesisUtterance(textVal);
-		msg.volume = 1;
-		msg.voice = voices[$("#voice").val()];
-		//msg.pitch = 0;
-
-		msg.onend = function (event) {
-			$("#speak-control-stop-me-if-you-can").hide();
-			$("#speak").show();
-		};
-		window.speechSynthesis.speak(msg);
-
-		$(this).hide();
-		$("#speak-control-stop-me-if-you-can").show();
-	});
-
-	$("#stop-speak").click(function () {
-		$("#speak-control-stop-me-if-you-can").hide();
-		$("#speak").show();
-		window.speechSynthesis.cancel();
-	});
-
-	// $("#pause-speak").click(function () {
-		
-	// });
 }
 
-function setAccessibilityFunctions () {
-	document.cookie = "font-normal-size=" + $("html").css("fontSize");
+function setSpeakFunction(voices) {
+    $("#speak").click(function(ev) {
+        var textVal = $("#text").val();
+        if (textVal === null || textVal === "") return;
 
-	$("#font-up").click(function () {
-		var valueFontSize = toNumber($("html").css("fontSize"));
-		$("html").css({fontSize: toPx(valueFontSize + 1)});
-	});
+        var msg = new SpeechSynthesisUtterance(textVal);
+        msg.volume = 1;
+        msg.voice = voices[$("#voice").val()];
+        //msg.pitch = 0;
 
-	$("#font-down").click(function () {
-		var valueFontSize = toNumber($("html").css("fontSize"));
-		$("html").css({fontSize: toPx(valueFontSize - 1)});
-	});
+        msg.onend = function(event) {
+            $("#speak-control-stop-me-if-you-can").hide();
+            $("#speak").show();
+        };
+        window.speechSynthesis.speak(msg);
 
-	$("#font-normal").click(function () {
-		$("html").css({fontSize: getCookieValue("font-normal-size")});
-	});	
+        $(this).hide();
+        $("#speak-control-stop-me-if-you-can").show();
+    });
 
-	$("#high-contrast").click(function () {
-		$("body").prop('class', "alto-contraste");
-	});
+    $("#stop-speak").click(function() {
+        $("#speak-control-stop-me-if-you-can").hide();
+        $("#speak").show();
+        window.speechSynthesis.cancel();
+    });
 
-	$("#sepia-contrast").click(function () {
-		$("body").prop('class', "sepia");
-	});
+    // $("#pause-speak").click(function () {
 
-	$("#normal-contrast").click(function () {
-		$("body").prop('class', "");
-	});
+    // });
+}
+
+function setAccessibilityFunctions() {
+    document.cookie = "font-normal-size=" + $("html").css("fontSize");
+
+    $("#font-up").click(function() {
+        var valueFontSize = toNumber($("html").css("fontSize"));
+        $("html").css({ fontSize: toPx(valueFontSize + 1) });
+    });
+
+    $("#font-down").click(function() {
+        var valueFontSize = toNumber($("html").css("fontSize"));
+        $("html").css({ fontSize: toPx(valueFontSize - 1) });
+    });
+
+    $("#font-normal").click(function() {
+        $("html").css({ fontSize: getCookieValue("font-normal-size") });
+    });
+
+    $("#high-contrast").click(function() {
+        $("body").prop('class', "alto-contraste");
+    });
+
+    $("#sepia-contrast").click(function() {
+        $("body").prop('class', "sepia");
+    });
+
+    $("#normal-contrast").click(function() {
+        $("body").prop('class', "");
+    });
 }
 
 function getCookieValue(name) {
-	var valueMatch = new RegExp(name + "=(.+?)(;|$)", "");
-	var results = document.cookie.match(valueMatch);
+    var valueMatch = new RegExp(name + "=(.+?)(;|$)", "");
+    var results = document.cookie.match(valueMatch);
 
-	return results[1];
+    return results[1];
 }
 
-function toNumber (string) {
-	return parseInt(string.replace(/[^\d\-\.]/g, ""), 10);
+function toNumber(string) {
+    return parseInt(string.replace(/[^\d\-\.]/g, ""), 10);
 }
 
-function toPx (intValue) {
-	return intValue + "px";
+function toPx(intValue) {
+    return intValue + "px";
 }
 
 function animate(elem, xAxisMax, xAxisMin) {
-	var timeId = null;
-	var step = 1;
-	$(elem).css({left: "0px"});
-	return {
-		start: function () {
-			var self = this;
-			timeId = setTimeout(function () {
-				var cleft = toNumber($(elem).css("left"));
-				step = (cleft == xAxisMax || cleft == xAxisMin) ? step * -1 : step; 
-				$(elem).css({left: toPx(cleft + step)});
-				///console.log("move:" + $(elem).css("left"));
-				self.start();
-			}, 50);
+    var timeId = null;
+    var step = 1;
+    $(elem).css({ left: "0px" });
+    return {
+        start: function() {
+            var self = this;
+            timeId = setTimeout(function() {
+                var cleft = toNumber($(elem).css("left"));
+                step = (cleft == xAxisMax || cleft == xAxisMin) ? step * -1 : step;
+                $(elem).css({ left: toPx(cleft + step) });
+                ///console.log("move:" + $(elem).css("left"));
+                self.start();
+            }, 50);
 
-			return true;
-		},	
+            return true;
+        },
 
-		end: function () {
-			if (timeId != null) {
-				clearTimeout(timeId);
-				timeId = null;
-				$(elem).css({left: "0px"});
-				return true;
-			}
+        end: function() {
+            if (timeId != null) {
+                clearTimeout(timeId);
+                timeId = null;
+                $(elem).css({ left: "0px" });
+                return true;
+            }
 
-			return false;
-		}
+            return false;
+        }
 
-	};
+    };
 }
 
-function speechRecognitionAPI () {
-	return window.webkitSpeechRecognition || window.SpeechRecognition;
+function speechRecognitionAPI() {
+    return window.webkitSpeechRecognition || window.SpeechRecognition;
 }
 
-function testAPIAvailability (f_yes, f_no) {
-	return (speechRecognitionAPI() == null) ? f_no() : f_yes();	
+function testAPIAvailability(f_yes, f_no) {
+    return (speechRecognitionAPI() == null) ? f_no() : f_yes();
 }
 
-function evalTranscript (string) {
-	var substitution = {
-		"ponto de interrogação" : "?",
-		"ponto de exclamação" : "!",
-		"vírgula": ",",
-		"ponto final": ".",
-		"ponto e vírgula": ";",
-		"reticências" : "...",
-		"nova linha" : "\n"
-	};
+function evalTranscript(string) {
+    var substitution = {
+        "ponto de interrogação": "?",
+        "ponto de exclamação": "!",
+        "vírgula": ",",
+        "ponto final": ".",
+        "ponto e vírgula": ";",
+        "reticências": "...",
+        "nova linha": "\n"
+    };
 
-	console.log(string);
+    console.log(string);
 
-	return (substitution[string.toLowerCase().trim()] || string);
+    return (substitution[string.toLowerCase().trim()] || string);
 }
 
-function loadVoices (f) {
-	if (loadVoices.voices !== undefined) {
-		return f(loadVoices.voices);
-	} else {
-		if (navigator.userAgent.toLowerCase().indexOf('chrome') > -1){
-			window.speechSynthesis.onvoiceschanged = function () {
-				var voices = window.speechSynthesis.getVoices();
-				loadVoices.voices = voices;
-				window.speechSynthesis.onvoiceschanged = null;
-				f(voices);
-			}
-		} else {
-			var voices = window.speechSynthesis.getVoices();
-			loadVoices.voices = voices;
-			f(voices);
-		}
+function loadVoices(f) {
+    if (loadVoices.voices !== undefined) {
+        return f(loadVoices.voices);
+    } else {
+        if (navigator.userAgent.toLowerCase().indexOf('chrome') > -1) {
+            window.speechSynthesis.onvoiceschanged = function() {
+                var voices = window.speechSynthesis.getVoices();
+                loadVoices.voices = voices;
+                window.speechSynthesis.onvoiceschanged = null;
+                f(voices);
+            }
+        } else {
+            var voices = window.speechSynthesis.getVoices();
+            loadVoices.voices = voices;
+            f(voices);
+        }
 
-		return null;
-	}
+        return null;
+    }
 }
 
-$(document).ready(function () {
-	setAccessibilityFunctions();
+$(document).ready(function() {
+    setAccessibilityFunctions();
 
-	var voicesLoaderFunction = function (voices) {
-		var selectVoices = document.querySelector("#voice");
-		selectVoices.innerHTML = Array.prototype.reduce.call(voices, function (acc, elm, idx) {
-		return  elm.lang.indexOf("pt") !== -1 ? 
-				 acc + "<option value=\"" + idx + "\">" 
-					  + elm.name + " (" + elm.lang + ")" + "</option>" : acc; 
-		}, "");
+    var voicesLoaderFunction = function(voices) {
+        var selectVoices = document.querySelector("#voice");
+        selectVoices.innerHTML = Array.prototype.reduce.call(voices, function(acc, elm, idx) {
+            return elm.lang.indexOf("pt") !== -1 ?
+                acc + "<option value=\"" + idx + "\">" +
+                elm.name + " (" + elm.lang + ")" + "</option>" : acc;
+        }, "");
 
-		setSpeakFunction(voices);
-	};
+        setSpeakFunction(voices);
+    };
 
-	var f_yes = function () {
-		$("#availability-info").hide();
-		setOptions();
-		$("#tts").change();
-		$("#stop-hear").parent().hide();
-		$("#hear").parent().show();
-		
-		loadVoices(voicesLoaderFunction);
-		
-		var recog = new webkitSpeechRecognition();
-		recog.lang = "pt-BR";
-		recog.continuous = true;
-		recog.interimResults = false;
+    var f_yes = function() {
+        $("#availability-info").hide();
+        setOptions();
+        $("#tts").change();
+        $("#stop-hear").parent().hide();
+        $("#hear").parent().show();
 
-		recog.onresult = function (ev) {
-			$("#text").val(Array.prototype.reduce.call(ev.results, function(acc, speechResult) {
-				return acc + evalTranscript(speechResult[0].transcript);
-			}, ""));
-			$("#text").focus();
-		}
+        loadVoices(voicesLoaderFunction);
 
-		var animObject = animate($(".bticon"), 10, -10);
+        var recog = new webkitSpeechRecognition();
+        recog.lang = "pt-BR";
+        recog.continuous = true;
+        recog.interimResults = false;
 
-		recog.onspeechstart = function (ev) {
-			console.log("speech start");
-			animObject.start();
-		}
+        recog.onresult = function(ev) {
+            $("#text").val(Array.prototype.reduce.call(ev.results, function(acc, speechResult) {
+                return acc + evalTranscript(speechResult[0].transcript);
+            }, ""));
+            $("#text").focus();
+        }
 
-		recog.onspeechend = function (ev) {
-			console.log("speech end");
-			animObject.end();
-		}
+        var animObject = animate($(".bticon"), 10, -10);
 
-		recog.onend = function () {
-			$("#stop-hear").trigger("click");
-		}
+        recog.onspeechstart = function(ev) {
+            console.log("speech start");
+            animObject.start();
+        }
 
-		recog.onerror = function (ev) {
-			console.log(ev.error + ": " + ev.message);
-		}
+        recog.onspeechend = function(ev) {
+            console.log("speech end");
+            animObject.end();
+        }
 
-		$("#stop-hear").click(function (ev) {
-			recog.stop();
-			$("#stop-hear").parent().hide();
-			$("#hear").parent().show();
-			animObject.end();
-		});
+        recog.onend = function() {
+            $("#stop-hear").trigger("click");
+        }
 
-		$("#hear").click(function (ev) {
-			recog.start();
-			$("#stop-hear").parent().show();
-			$("#hear").parent().hide();
-		});
-	};
+        recog.onerror = function(ev) {
+            console.log(ev.error + ": " + ev.message);
+        }
 
-	var f_no = function () {
-		//$("#content").hide();
-		$("#tts").change();
-		$("#option-selector").hide();
-		$("#availability-info").show();
+        $("#stop-hear").click(function(ev) {
+            recog.stop();
+            $("#stop-hear").parent().hide();
+            $("#hear").parent().show();
+            animObject.end();
+        });
 
-		loadVoices(voicesLoaderFunction);
-	}
+        $("#hear").click(function(ev) {
+            recog.start();
+            $("#stop-hear").parent().show();
+            $("#hear").parent().hide();
+        });
+    };
 
-	testAPIAvailability(f_yes, f_no);
+    var f_no = function() {
+        //$("#content").hide();
+        $("#tts").change();
+        $("#option-selector").hide();
+        $("#availability-info").show();
 
-	
+        loadVoices(voicesLoaderFunction);
+    }
+
+    testAPIAvailability(f_yes, f_no);
+
+
 });
